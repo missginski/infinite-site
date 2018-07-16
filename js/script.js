4b6bcf4670be14e4cec0532771929cf8efba408c
Kristyn Ginski <missginski@gmail.com>
2018-07-16 14:25:52 -0400

    finalize section layout

--- js/script.js
+++ js/script.js
@@ -14,34 +14,20 @@
         }
       });
 
-
-      var $grid = $('.grid').isotope({
+      let $grid = $('.grid').isotope({
         itemSelector: '.grid-item',
         layoutMode: 'masonry'
       });
-      // filter functions
-      var filterFns = {
-        // show if number is greater than 50
-        numberGreaterThan50: function() {
-          var number = $(this).find('.number').text();
-          return parseInt( number, 10 ) > 50;
-        },
-        // show if name ends with -ium
-        ium: function() {
-          var name = $(this).find('.name').text();
-          return name.match( /ium$/ );
-        }
-      };
+      let filterFns = {};
       // bind filter button click
       $('.filters-button-group').on( 'click', 'button', function() {
-        var filterValue = $( this ).attr('data-filter');
-        // use filterFn if matches value
+        let filterValue = $( this ).attr('data-filter');
         filterValue = filterFns[ filterValue ] || filterValue;
         $grid.isotope({ filter: filterValue });
       });
       // change is-checked class on buttons
       $('.button-group').each( function( i, buttonGroup ) {
-        var $buttonGroup = $( buttonGroup );
+        let $buttonGroup = $( buttonGroup );
         $buttonGroup.on( 'click', 'button', function() {
           $buttonGroup.find('.is-checked').removeClass('is-checked');
           $( this ).addClass('is-checked');
